0b930a3d5ba91bdc0bf06f591f46ccca53a3b1cb
zakarialbazri <125029888+zakarialbazri@users.noreply.github.com>
2023-06-26 20:24:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ import Menu from "./components/menu";
 
 
 const App = () => { 
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:8080/hello')
-      .then(response => {
-        setMessage(response.data);
-      })
-      .catch(error => {
-        if (error.message === 'Network Error') {
-          setError('Unable to connect to the server. Please ensure it is running.');
-        } else {
-          setError('An error occurred while fetching the data.');
-        }
-      });
-  }, []);
+  
 
   return (
    <div>
